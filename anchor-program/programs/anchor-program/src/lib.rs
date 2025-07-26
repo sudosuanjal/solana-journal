7867c44c3f0b4078bae80b26a0216d8c482966d8
sudosuanjal <anjalehackchey101@gmail.com>
2025-07-26 20:54:41 +0530

    feat(achor): create journal entry

--- anchor-program/programs/anchor-program/src/lib.rs
+++ anchor-program/programs/anchor-program/src/lib.rs
@@ -5,12 +5,39 @@ declare_id!("4JMXT6oZZjNk7Hq1ha55c9Ghx8gykYt9bV1thMk8uu9S");
 #[program]
 pub mod anchor_program {
     use super::*;
+    pub fn create_journal_entry(ctx:Context<CreateEntry>, title:String, message:String)->Result<()>{
+      msg!("journal entry created!");
+      msg!("title: {}", title);
+      msg!("message: {}", message);
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+      let journal_entry = &mut ctx.accounts.journal_entry;
+      journal_entry.owner = ctx.accounts.owner.key();
+      journal_entry.title = title;
+      journal_entry.message = message;
+
+      Ok(())
     }
 }
 
+#[account]
+pub struct JournalEntryState{
+  pub owner:Pubkey,
+  pub title:String,
+  pub message:String,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(title:String,message:String)]
+pub struct CreateEntry<'info>{
+  #[account(
+    init,
+    seeds = [title.as_bytes(), owner.key().as_ref()],
+    bump,
+    payer = owner,
+    space = 8 + 32 + 4 + title.len() + message.len()
+  )]
+  pub journal_entry: Account<'info, JournalEntryState>,
+  #[account(mut)]
+  pub owner:Signer<'info>,
+  pub system_program:Program<'info, System>
+}
