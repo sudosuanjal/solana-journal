4e2d73b056467d20b2b68eb48b2396a048abe8b1
sudosuanjal <anjalehackchey101@gmail.com>
2025-07-28 11:41:06 +0530

    get program in the client

--- frontend/src/components/JournalApp.tsx
+++ frontend/src/components/JournalApp.tsx
@@ -1,10 +1,31 @@
 "use client";
-import { useWallet } from "@solana/wallet-adapter-react";
+import { getProgram } from "@/utils/program";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const JournalApp = () => {
-  const { connected, publicKey } = useWallet();
+  const { connected, publicKey, wallet } = useWallet();
+  const connecttion = useConnection();
+  const [result, setResult] = useState<string | null>("waiting...");
+
+  useEffect(() => {
+    const fetchProgram = async () => {
+      if (connected && publicKey && wallet) {
+        const { connection } = connecttion;
+        const program = getProgram(connection, wallet.adapter as any);
+        try {
+          setResult(`Program loaded! ID: ${program.programId.toBase58()}`);
+        } catch (error) {
+          setResult("Failed to load program.");
+        }
+      } else {
+        setResult("Please connect your wallet.");
+      }
+    };
+
+    fetchProgram();
+  }, [wallet, publicKey, connecttion]);
 
   return (
     <div className="p-4">
@@ -15,6 +36,7 @@ const JournalApp = () => {
           Connected: {publicKey?.toBase58()}
         </p>
       )}
+      <p className="text-gray-700">{result}</p>
     </div>
   );
 };
