8ecdb4a49d9c27892c7e842bc7727681545a1311
sudosuanjal <anjalehackchey101@gmail.com>
2025-07-29 23:38:51 +0530

    fix: added the missing space for message

--- anchor-program/programs/anchor-program/src/lib.rs
+++ anchor-program/programs/anchor-program/src/lib.rs
@@ -5,88 +5,87 @@ declare_id!("4JMXT6oZZjNk7Hq1ha55c9Ghx8gykYt9bV1thMk8uu9S");
 #[program]
 pub mod anchor_program {
     use super::*;
-    pub fn create_journal_entry(ctx:Context<CreateEntry>, title:String, message:String)->Result<()>{
-      msg!("journal entry created!");
-      msg!("title: {}", title);
-      msg!("message: {}", message);
+    pub fn create_journal_entry(ctx: Context<CreateEntry>, title: String, message: String) -> Result<()> {
+        msg!("journal entry created!");
+        msg!("title: {}", title);
+        msg!("message: {}", message);
 
-      let journal_entry = &mut ctx.accounts.journal_entry;
-      journal_entry.owner = ctx.accounts.owner.key();
-      journal_entry.title = title;
-      journal_entry.message = message;
+        let journal_entry = &mut ctx.accounts.journal_entry;
+        journal_entry.owner = ctx.accounts.owner.key();
+        journal_entry.title = title;
+        journal_entry.message = message;
 
-      Ok(())
+        Ok(())
     }
 
-    pub fn update_journal_entry(ctx:Context<UpdateEntry>, title:String, message:String)->Result<()>{
+    pub fn update_journal_entry(ctx: Context<UpdateEntry>, title: String, message: String) -> Result<()> {
         msg!("journal entry updated");
         msg!("title: {}", title);
         msg!("message: {}", message);
-  
+
         let journal_entry = &mut ctx.accounts.journal_entry;
         journal_entry.message = message;
         Ok(())
-      }
+    }
 
-      pub fn delete_journal_entry(_ctx:Context<DeleteEntry>, title:String)->Result<()>{
+    pub fn delete_journal_entry(_ctx: Context<DeleteEntry>, title: String) -> Result<()> {
         msg!("journal entry deleted");
         msg!("title: {}", title);
         Ok(())
-      }
+    }
 }
 
 #[account]
-pub struct JournalEntryState{
-  pub owner:Pubkey,
-  pub title:String,
-  pub message:String,
+pub struct JournalEntryState {
+    pub owner: Pubkey,
+    pub title: String,
+    pub message: String,
 }
 
 #[derive(Accounts)]
-#[instruction(title:String,message:String)]
-pub struct CreateEntry<'info>{
-  #[account(
-    init,
-    seeds = [title.as_bytes(), owner.key().as_ref()],
-    bump,
-    payer = owner,
-    space = 8 + 32 + 4 + title.len() + message.len()
-  )]
-  pub journal_entry: Account<'info, JournalEntryState>,
-  #[account(mut)]
-  pub owner:Signer<'info>,
-  pub system_program:Program<'info, System>
+#[instruction(title: String, message: String)]
+pub struct CreateEntry<'info> {
+    #[account(
+        init,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        payer = owner,
+        space = 8 + 32 + 4 + title.len() + 4 + message.len() 
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-#[instruction(title:String, message:String)]
-pub struct UpdateEntry<'info>{
-  #[account(
-    mut,
-    seeds = [title.as_bytes(), owner.key().as_ref()],
-    bump,
-    realloc = 8 + 32 + 4 + title.len() + 4 + message.len(),
-    realloc::payer = owner, 
-    realloc::zero = true, 
-
-  )]
-  pub journal_entry:Account<'info, JournalEntryState>,
-  #[account(mut)]
-  pub owner:Signer<'info>,
-  pub system_program:Program<'info, System>
+#[instruction(title: String, message: String)]
+pub struct UpdateEntry<'info> {
+    #[account(
+        mut,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        realloc = 8 + 32 + 4 + title.len() + 4 + message.len(),
+        realloc::payer = owner,
+        realloc::zero = true,
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-#[instruction(title:String)]
-pub struct DeleteEntry<'info>{
- #[account(
-  mut,
-  seeds = [title.as_bytes(), owner.key().as_ref()],
-  bump,
-  close = owner
- )]
- pub journal_entry:Account<'info, JournalEntryState>,
- #[account(mut)]
- pub owner:Signer<'info>,
- pub system_program:Program<'info, System>
+#[instruction(title: String)]
+pub struct DeleteEntry<'info> {
+    #[account(
+        mut,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        close = owner
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
