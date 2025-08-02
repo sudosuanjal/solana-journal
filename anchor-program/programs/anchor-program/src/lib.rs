2a5cd278cdda8ddba7f93a10dabbb67199620ed8
sudosuanjal <anjalehackchey101@gmail.com>
2025-08-03 00:02:51 +0530

    feat:added mood and created date to the program

--- anchor-program/programs/anchor-program/src/lib.rs
+++ anchor-program/programs/anchor-program/src/lib.rs
@@ -5,26 +5,33 @@ declare_id!("4JMXT6oZZjNk7Hq1ha55c9Ghx8gykYt9bV1thMk8uu9S");
 #[program]
 pub mod anchor_program {
     use super::*;
-    pub fn create_journal_entry(ctx: Context<CreateEntry>, title: String, message: String) -> Result<()> {
+
+    pub fn create_journal_entry(ctx: Context<CreateEntry>, title: String, message: String, mood:Mood) -> Result<()> {
         msg!("journal entry created!");
         msg!("title: {}", title);
         msg!("message: {}", message);
+        msg!("mood: {:?}", mood);
 
         let journal_entry = &mut ctx.accounts.journal_entry;
         journal_entry.owner = ctx.accounts.owner.key();
         journal_entry.title = title;
         journal_entry.message = message;
+        journal_entry.mood = mood;
+        journal_entry.created_at = Clock::get()?.unix_timestamp;
 
         Ok(())
     }
 
-    pub fn update_journal_entry(ctx: Context<UpdateEntry>, title: String, message: String) -> Result<()> {
+    pub fn update_journal_entry(ctx: Context<UpdateEntry>, title: String, message: String, mood:Mood) -> Result<()> {
         msg!("journal entry updated");
         msg!("title: {}", title);
         msg!("message: {}", message);
+        msg!("mood: {:?}", mood);
 
         let journal_entry = &mut ctx.accounts.journal_entry;
         journal_entry.message = message;
+        journal_entry.mood = mood;
+        journal_entry.updated_at = Some(Clock::get()?.unix_timestamp);
         Ok(())
     }
 
@@ -35,22 +42,35 @@ pub mod anchor_program {
     }
 }
 
+#[derive(AnchorSerialize, AnchorDeserialize,Clone,Debug,PartialEq)]
+pub enum  Mood {
+    Awesome,
+    Happy, 
+    Okay,
+    Bad,
+    Terrible,
+}
+
 #[account]
 pub struct JournalEntryState {
     pub owner: Pubkey,
     pub title: String,
     pub message: String,
+    pub mood: Mood,
+    pub created_at: i64,
+    pub updated_at:Option<i64>
 }
 
+
 #[derive(Accounts)]
-#[instruction(title: String, message: String)]
+#[instruction(title: String, message: String, mood:Mood)]
 pub struct CreateEntry<'info> {
     #[account(
         init,
         seeds = [title.as_bytes(), owner.key().as_ref()],
         bump,
         payer = owner,
-        space = 8 + 32 + 4 + title.len() + 4 + message.len() 
+        space = 8 + 32 + 4 + title.len() + 4 + message.len() + 1 + 8 + 9
     )]
     pub journal_entry: Account<'info, JournalEntryState>,
     #[account(mut)]
@@ -59,13 +79,13 @@ pub struct CreateEntry<'info> {
 }
 
 #[derive(Accounts)]
-#[instruction(title: String, message: String)]
+#[instruction(title: String, message: String, mood:Mood)]
 pub struct UpdateEntry<'info> {
     #[account(
         mut,
         seeds = [title.as_bytes(), owner.key().as_ref()],
         bump,
-        realloc = 8 + 32 + 4 + title.len() + 4 + message.len(),
+        realloc = 8 + 32 + 4 + title.len() + 4 + message.len() + 1 + 8 + 9,
         realloc::payer = owner,
         realloc::zero = true,
     )]
